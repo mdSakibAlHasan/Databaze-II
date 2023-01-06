0570a58e8ee62708f9703a0227f17e59cfff6d30
mdSakibAlHasan <bsse1209@iit.du.ac.bd>
2023-01-06 22:29:37 +0600

    concurrency added in in c++ code

--- Others/concurency.cpp
+++ Others/concurency.cpp
@@ -1,74 +1,134 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX_TRAN 10
-#define VARIABLE 2
-char vari_data[VARIABLE] = {'x', 'y'};
 
-void store_in_array(char temp[], int tran[VARIABLE][2], int *current_variable, int num)
+#define MAX 100
+#define initial 1
+#define visited 2
+#define finished 3
+
+int n; /*Number of vertices in the graph */
+int adj[MAX][MAX];
+void create_graph();
+
+int state[MAX];
+
+vector<pair<bool, pair<char, int>>> info;
+
+void store_in_array(char temp[])
 {
-    cout << temp << endl;
-    int index = 0, i;
-    char x = temp[4];
-    for (i = 0; i < VARIABLE; i++)
+    char variable;
+    bool read;
+    int transaction_number;
+    stringstream ss;
+    for (int i = strlen(temp) - 1; i >= 0; i--)
     {
-        if (x == vari_data[i])
+        if (temp[i] == '(' || temp[i] == ')')
+            continue;
+        else if (temp[i] == 'w' || temp[i] == 'r')
         {
-            index = i;
-            break;
+            if (temp[i] == 'w')
+                read = false;
+            else
+                read = true;
+        }
+        else if (isdigit(temp[i]))
+        {
+            ss << temp[i];
+        }
+        else
+        {
+            variable = temp[i];
         }
     }
+    ss >> transaction_number;
 
-    if (temp[0] == 'r')
-    {
-        tran[num++][0] = 0;
-        tran[num++][1] = temp[1] - '0';
-        cout << " " << temp[1] << " ";
-    }
-    else
-    {
-        tran[num++][0] = 1;
-        tran[num++][1] = temp[1] - '0';
-    }
+    info.push_back(make_pair(read, make_pair(variable, transaction_number)));
 }
 
-void split_input(char str[], int tran[VARIABLE][2], int num)
+void split_input(char str[])
 {
-    // cout << num[0] << num[0] << endl;
-    bool read = false;
     char temp[8];
-    int word_number = 0, current_variable = 0;
+    int word_number = 0;
     for (int i = 0; i < strlen(str); i++)
     {
-        // cout << x << " ";
         if (str[i] == ' ')
         {
             temp[word_number] = '\0';
             word_number = 0;
-            store_in_array(temp, tran, &current_variable, num);
+            store_in_array(temp);
         }
         else
         {
             temp[word_number++] = str[i];
         }
     }
-    // char *ptr = strtok(str, ",");
+}
 
-    // while (ptr != NULL)
-    // {
-    //     store_in_array(temp, tran, &current_variable, num);
-    //     ptr = strtok(NULL, ",");
-    // }
+void DFS(int v)
+{
+    int i;
+    state[v] = visited;
+
+    for (i = 0; i < n; i++)
+    {
+        if (adj[v][i] == 1)
+        {
+            if (state[i] == initial)
+                DFS(i);
+            else if (state[i] == visited)
+            {
+                printf("\nGraph is cyclic\n");
+                exit(1);
+            }
+        }
+    }
+    state[v] = finished;
+}
+
+void DF_Traversal()
+{
+    int v;
+
+    for (v = 0; v < n; v++)
+        state[v] = initial;
+
+    DFS(0);
+
+    for (v = 0; v < n; v++)
+    {
+        if (state[v] == initial)
+            DFS(v);
+    }
+    printf("\nGraph is Acyclic\n");
+}
+
+void make_graph()
+{
+    int max_edges, origin, destin;
+
+    for (int i = 0; i < info.size(); i++)
+    {
+        bool read = true;
+        for (int j = i + 1; j < info.size(); j++)
+        {
+            if (info[i].second.first == info[j].second.first)
+            {
+                if (!info[i].first || !info[j].first)
+                    read = false;
+                if (!read)
+                {
+                    adj[info[i].second.second][info[j].second.second] = 1;
+                }
+            }
+        }
+    }
 }
 
 int main()
 {
     char input[] = "r2(x) r3(y) w3(x) r1(y) w2(x) w1(y) w1(x) ";
-    int transaction[VARIABLE][2];
-    int number_of_transaction = 0, total;
-    split_input(input, transaction, &number_of_transaction);
-
-    for (int i = 0; i < number_of_transaction; i++)
-    {
-        cout << transaction[i][0] << " " << transaction[i][1] << " --- ";
-    }
+    n = 3;
+    split_input(input);
+    make_graph();
+    DF_Traversal();
 }
